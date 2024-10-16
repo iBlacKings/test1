0f15cee1f06a274dc01ad44516614b0a717705c6
iBlacKings <juangilagudelo@correounivalle.edu.co>
2024-10-15 22:22:13 -0500

    v0.2

--- helloworld.py
+++ helloworld.py
@@ -5,10 +5,20 @@ if __name__ == "__main__":
     
     #git status = know if the files are commited
     
-    #git add = decide which files are going to go to the commit
+    #git add = decide with files are going to the stage that then go to the commit
     
     #git commit = save a state of the project in a specific moment like a photo
     
+    #git checkout = can be used to move between commints and restoring by the file name to the last commit that we made it 
+    
+    #git reset (--hard --soft --mixed) == --hard(with this we delete all, the commit and also the changes that we do there)
+                                        # --soft(with this we only remove the commit but we have all the files on the stage we 
+                                        # can add and modifed the mesagge of the commit but we can modified the files that already
+                                        # load in the stage)
+                                        # --mixed(is too similar to the soft but here the files aren't in the stage so we can modifed but we
+                                        # need to add it to the stage with git add. )
+    
+    #git tag
     
     
     print("Hello world")
