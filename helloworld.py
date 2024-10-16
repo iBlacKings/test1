e3f3016b7e576bebb0e2024b042491c2dcfa8be3
iBlacKings <juangilagudelo@correounivalle.edu.co>
2024-10-15 21:43:24 -0500

    v0.1

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+
+if __name__ == "__main__":
+    
+    #git init = Starts git on a project
+    
+    #git status = know if the files are commited
+    
+    #git add = decide which files are going to go to the commit
+    
+    #git commit = save a state of the project in a specific moment like a photo
+    
+    
+    
+    print("Hello world")
+    
